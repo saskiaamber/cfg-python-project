69da672e40c49eed624571ff011717b872aa0794
Saskia Lawson-Tovey <saskialawsontovey@gmail.com>
2018-03-14 18:27:43 +0000

    Week4 changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask("my_first_app")
 
@@ -6,5 +6,20 @@ app = Flask("my_first_app")
 def say_hello():
 	return render_template("index.html")
 
-app.run(debug=True)
+@app.route("/me")
+def say_me():
+	return render_template("new.html")
 
+@app.route("/<name>")
+def say_hello_to(name):
+	return render_template("hello.html", user=name)
+
+@app.route("/feedback", methods=["POST"])
+def get_feedback():
+	# request.values is a dictionary holding any POST request data that's not already part of the URL
+	data = request.values
+
+	return render_template("feedback.html", form_data=data)
+
+
+app.run(debug=True)
